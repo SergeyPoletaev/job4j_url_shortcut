7a232ab44e0f3cd51a334db198fea6b3f7b920f2
spoletaev <info.elekit@yandex.ru>
2023-08-01 19:49:24 +0300

    добавить логирование

--- src/main/java/ru/job4j/url/shortcut/controller/UrlController.java
+++ src/main/java/ru/job4j/url/shortcut/controller/UrlController.java
@@ -1,6 +1,7 @@
 package ru.job4j.url.shortcut.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
@@ -22,11 +23,13 @@ import javax.validation.constraints.Size;
 @RestController
 @RequiredArgsConstructor
 @Validated
+@Slf4j
 public class UrlController {
     private final UrlService urlService;
 
     @PostMapping("/convert")
     public ResponseEntity<CodeDto> convert(@RequestBody @Valid UrlDto urlDto) {
+        log.info("Регистрация URL ==> {}", urlDto.getUrl());
         Url url = new Url().setLink(urlDto.getUrl());
         urlService.save(url);
         return ResponseEntity.ok().body(new CodeDto().setCode(url.getCode()));
@@ -34,6 +37,7 @@ public class UrlController {
 
     @GetMapping("/redirect/{code}")
     public ResponseEntity<Void> redirect(@PathVariable @Size(min = 10, max = 10) String code) {
+        log.info("Запрос URL по коду ==> {}", code);
         Url url = urlService.findByCode(code).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         String.format("URL address with code [%s] not found", code)));
@@ -46,6 +50,7 @@ public class UrlController {
                                                            @Min(0) int pageNum,
                                                            @RequestParam(value = "sizePage", defaultValue = "20")
                                                            @Min(1) @Max(100) int sizePage) {
+        log.info("Запрос статистики переходов");
         Page<Url> page = urlService.findAll(PageRequest.of(pageNum, sizePage));
         Page<StatisticDto> statisticDtoPage = page.map(url -> new StatisticDto().setUrl(url.getLink()).setTotal(url.getTotal()));
         return ResponseEntity.ok().body(statisticDtoPage);
